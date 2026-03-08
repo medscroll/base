98d246ca3292c16783b28bad97483f42da1357cf
medscroll <medscroll2025@gmail.com>
2026-03-08 15:24:22 +0100

    Add premiumUsers array to EcoUsers.js

---
+++ EcoUsers.js
@@ -0,0 +1,7 @@
+const premiumUsers = [
+            "NSIETE BERMIE",
+            "KAMBA TRIOMPHE",
+            "MED SCROLL",
+            "NZINGA EMMANUEL",
+            "ILUNGA GRACE"
+            ];
